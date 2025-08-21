7c6e77f329ed7cfbd373be7a38e43bbaf253604b
equinoxtian <equinoxtian@tencent.com>
2025-08-21 15:51:29 +0800

    add web_api_demo: history.popstate

---
+++ web_utilities/web_api_demos/history_popstate/main.js
@@ -0,0 +1,39 @@
+function pushHistory() {
+  var state = {
+    title: 'title',
+    url: '#'
+  };
+  window.history.pushState(state, 'title', '#');
+}
+
+function listenNavBack(callback) {
+  pushHistory();
+
+  var bool = false;
+  setTimeout(function() {
+    bool = true;
+  }, 1500);
+
+  window.addEventListener(
+    'popstate',
+    function(e) {
+      if (bool) {
+        callback(e);
+      }
+      pushHistory();
+    },
+    false
+  );
+};
+
+function _initNavBack() {
+  listenNavBack(() => {
+    alert('回退拦截');
+  });
+}
+
+function ua() {
+  return navigator.userAgent;
+}
+
+_initNavBack();
